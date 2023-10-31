76e8db65d50044bba2534cdb36dcc2936fc38b98
Suiane <maria.suine934@gmail.com>
2023-10-31 09:30:49 -0300

    ResponsiveVoice

--- app.js
+++ app.js
@@ -64,6 +64,7 @@ let tentativas = 1;
 function exibirTextoNaTela(tag, texto) {
   let campo = document.querySelector(tag);
   campo.innerHTML = texto;
+  responsiveVoice.speak(texto, 'Brazilian Portuguese Female' ,{rate:1.2})
 }
 
 function exibirMensagemInicial() {
